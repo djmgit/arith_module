e28b5467af9c2e841c202b899a7baf0eae1713d4
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2016-10-31 12:17:09 +0530

    Added new methods to module.c

--- arith/module.c
+++ arith/module.c
@@ -216,7 +216,7 @@ int Pow(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 
 int LincrBy(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 
- // we must have at least 4 args
+ // we must have at least 5 args
   if (argc != 5) {
     return RedisModule_WrongArity(ctx);
   }
@@ -227,13 +227,8 @@ int LincrBy(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   RedisModuleCallReply *rep =
       RedisModule_Call(ctx, "LLEN", "s", argv[1]);
   RMUTIL_ASSERT_NOERROR(rep);
-  //RedisModuleString *temp = RedisModule_CreateStringFromCallReply(rep);
-  //long long temp_val;
-  //RedisModule_StringToLongLong(temp,&temp_val);
-  //RedisModule_ReplyWithLongLong(ctx,temp_val);
   long long listLength;
   listLength = RedisModule_CallReplyInteger(rep);
-  //RedisModule_ReplyWithLongLong(ctx,listLength);
   long long listEnd = listLength - 1;
   if(RedisModule_StringToLongLong(argv[2], &val) != REDISMODULE_OK) {
   	RedisModule_ReplyWithError(ctx, "INVALID INCREMENT VALUE");
@@ -253,7 +248,7 @@ int LincrBy(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   }
 
   if(startIndex > endIndex && endIndex != -1) {
-  	RedisModule_ReplyWithError(ctx,"ENDINDEX SHOULD BE LESS THAN OR EQUAL TO ENDINDEX");
+  	RedisModule_ReplyWithError(ctx,"STARTINDEX SHOULD BE LESS THAN OR EQUAL TO ENDINDEX (EXCEPT WHEN ENDINDEX != -1");
   }
 
   if(endIndex == -1) {
@@ -278,6 +273,290 @@ int LincrBy(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   return REDISMODULE_OK;
 }
 
+// Method to calculate sum of a range of elements of a list
+
+int Lrsum(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+
+ // we must have at least 4 args
+  if (argc < 4) {
+    return RedisModule_WrongArity(ctx);
+  }
+
+  // init auto memory for created strings
+  RedisModule_AutoMemory(ctx);
+  long long startIndex, endIndex, rsum = 0;
+  RedisModuleCallReply *rep =
+      RedisModule_Call(ctx, "LLEN", "s", argv[1]);
+  RMUTIL_ASSERT_NOERROR(rep);
+  long long listLength;
+  listLength = RedisModule_CallReplyInteger(rep);
+  long long listEnd = listLength - 1;
+  if(RedisModule_StringToLongLong(argv[2], &startIndex) != REDISMODULE_OK) {
+  	RedisModule_ReplyWithError(ctx, "INVALID START INDEX");
+  }
+  if(RedisModule_StringToLongLong(argv[3], &endIndex) != REDISMODULE_OK) {
+  	RedisModule_ReplyWithError(ctx, "INVALID END INDEX");
+  }
+  if(startIndex < 0 || endIndex < -1) {
+  	RedisModule_ReplyWithError(ctx,"STARTINDEX CANNOT BE NEGATIVE AND ENDINDEX CANNOT BE LESS THAN -1");
+  }
+
+  if(startIndex > listEnd || endIndex > listEnd) {
+  	RedisModule_ReplyWithError(ctx,"INDEX POSITION OUT OF LIMIT");
+  }
+
+  if(startIndex > endIndex && endIndex != -1) {
+  	RedisModule_ReplyWithError(ctx,"STARTINDEX SHOULD BE LESS THAN OR EQUAL TO ENDINDEX (EXCEPT WHEN ENDINDEX != -1");
+  }
+
+  if(endIndex == -1) {
+  	endIndex = listEnd;
+  }
+
+  for(long long i = startIndex;i <= endIndex;i++) {
+
+    RedisModuleString *index = RedisModule_CreateStringFromLongLong(ctx, i);
+  	rep =
+      RedisModule_Call(ctx, "LINDEX", "ss", argv[1], index);
+    RMUTIL_ASSERT_NOERROR(rep);
+    long long element = GetArgVal(ctx, rep);  
+    rsum = rsum + element;
+  }
+
+  if(argc == 5) {
+  	rep =
+      RedisModule_Call(ctx, "SET", "sl", argv[4],rsum);
+    RMUTIL_ASSERT_NOERROR(rep);  
+  }
+  RedisModule_ReplyWithLongLong(ctx, rsum);
+  return REDISMODULE_OK;
+}
+
+// Method to find the largest element in a specified range in a list
+
+int Lrmax(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+
+ // we must have at least 4 args
+  if (argc < 4) {
+    return RedisModule_WrongArity(ctx);
+  }
+
+  // init auto memory for created strings
+  RedisModule_AutoMemory(ctx);
+  long long startIndex, endIndex, max = 0;
+  RedisModuleCallReply *rep =
+      RedisModule_Call(ctx, "LLEN", "s", argv[1]);
+  RMUTIL_ASSERT_NOERROR(rep);
+  long long listLength;
+  listLength = RedisModule_CallReplyInteger(rep);
+  long long listEnd = listLength - 1;
+  if(RedisModule_StringToLongLong(argv[2], &startIndex) != REDISMODULE_OK) {
+  	RedisModule_ReplyWithError(ctx, "INVALID START INDEX");
+  }
+  if(RedisModule_StringToLongLong(argv[3], &endIndex) != REDISMODULE_OK) {
+  	RedisModule_ReplyWithError(ctx, "INVALID END INDEX");
+  }
+  if(startIndex < 0 || endIndex < -1) {
+  	RedisModule_ReplyWithError(ctx,"STARTINDEX CANNOT BE NEGATIVE AND ENDINDEX CANNOT BE LESS THAN -1");
+  }
+
+  if(startIndex > listEnd || endIndex > listEnd) {
+  	RedisModule_ReplyWithError(ctx,"INDEX POSITION OUT OF LIMIT");
+  }
+
+  if(startIndex > endIndex && endIndex != -1) {
+  	RedisModule_ReplyWithError(ctx,"STARTINDEX SHOULD BE LESS THAN OR EQUAL TO ENDINDEX (EXCEPT WHEN ENDINDEX != -1");
+  }
+
+  if(endIndex == -1) {
+  	endIndex = listEnd;
+  }
+
+  for(long long i = startIndex;i <= endIndex;i++) {
+
+    RedisModuleString *index = RedisModule_CreateStringFromLongLong(ctx, i);
+  	rep =
+      RedisModule_Call(ctx, "LINDEX", "ss", argv[1], index);
+    RMUTIL_ASSERT_NOERROR(rep);
+    long long element = GetArgVal(ctx, rep);  
+    if(i == startIndex) {
+    	max = element;
+    }
+    else {
+    	max = element > max ? element : max;
+    }
+  }
+
+  if(argc == 5) {
+  	rep =
+      RedisModule_Call(ctx, "SET", "sl", argv[4],max);
+    RMUTIL_ASSERT_NOERROR(rep);  
+  }
+  RedisModule_ReplyWithLongLong(ctx, max);
+  return REDISMODULE_OK;
+}
+
+// Method to find the smallest element in a specified range in a list.
+
+int Lrmin(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+
+ // we must have at least 4 args
+  if (argc < 4) {
+    return RedisModule_WrongArity(ctx);
+  }
+
+  // init auto memory for created strings
+  RedisModule_AutoMemory(ctx);
+  long long startIndex, endIndex, min = 0;
+  RedisModuleCallReply *rep =
+      RedisModule_Call(ctx, "LLEN", "s", argv[1]);
+  RMUTIL_ASSERT_NOERROR(rep);
+  long long listLength;
+  listLength = RedisModule_CallReplyInteger(rep);
+  long long listEnd = listLength - 1;
+  if(RedisModule_StringToLongLong(argv[2], &startIndex) != REDISMODULE_OK) {
+  	RedisModule_ReplyWithError(ctx, "INVALID START INDEX");
+  }
+  if(RedisModule_StringToLongLong(argv[3], &endIndex) != REDISMODULE_OK) {
+  	RedisModule_ReplyWithError(ctx, "INVALID END INDEX");
+  }
+  if(startIndex < 0 || endIndex < -1) {
+  	RedisModule_ReplyWithError(ctx,"STARTINDEX CANNOT BE NEGATIVE AND ENDINDEX CANNOT BE LESS THAN -1");
+  }
+
+  if(startIndex > listEnd || endIndex > listEnd) {
+  	RedisModule_ReplyWithError(ctx,"INDEX POSITION OUT OF LIMIT");
+  }
+
+  if(startIndex > endIndex && endIndex != -1) {
+  	RedisModule_ReplyWithError(ctx,"STARTINDEX SHOULD BE LESS THAN OR EQUAL TO ENDINDEX (EXCEPT WHEN ENDINDEX != -1)");
+  }
+
+  if(endIndex == -1) {
+  	endIndex = listEnd;
+  }
+
+  for(long long i = startIndex;i <= endIndex;i++) {
+
+    RedisModuleString *index = RedisModule_CreateStringFromLongLong(ctx, i);
+  	rep =
+      RedisModule_Call(ctx, "LINDEX", "ss", argv[1], index);
+    RMUTIL_ASSERT_NOERROR(rep);
+    long long element = GetArgVal(ctx, rep);  
+    if(i == startIndex) {
+    	min = element;
+    }
+    else {
+    	min = element < min ? element : min;
+    }
+  }
+
+  if(argc == 5) {
+  	rep =
+      RedisModule_Call(ctx, "SET", "sl", argv[4],min);
+    RMUTIL_ASSERT_NOERROR(rep);  
+  }
+  RedisModule_ReplyWithLongLong(ctx, min);
+  return REDISMODULE_OK;
+}
+
+// Method to find the sum of the corresponding elements of two lists and store the result in a new list.
+
+int Llsum(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+
+ // we must have at least 4 args
+  if (argc < 4) {
+    return RedisModule_WrongArity(ctx);
+  }
+
+  // init auto memory for created strings
+  long long listLength1, listLength2;
+  RedisModule_AutoMemory(ctx);
+  RedisModuleCallReply *rep =
+      RedisModule_Call(ctx, "LLEN", "s", argv[1]);
+  RMUTIL_ASSERT_NOERROR(rep);
+  listLength1 = RedisModule_CallReplyInteger(rep);
+  rep =
+      RedisModule_Call(ctx, "LLEN", "s", argv[2]);
+  RMUTIL_ASSERT_NOERROR(rep);
+  listLength2 = RedisModule_CallReplyInteger(rep);
+
+  if(listLength1 != listLength2) {
+  	RedisModule_ReplyWithError(ctx,"LENGTH OF BOTH THE LISTS SHOULD BE EQUAL");
+  }
+  
+  for(long long i = 0;i < listLength1;i++) {
+
+    RedisModuleString *index = RedisModule_CreateStringFromLongLong(ctx, i);
+  	rep =
+      RedisModule_Call(ctx, "LINDEX", "ss", argv[1], index);
+    RMUTIL_ASSERT_NOERROR(rep);
+    long long element1 = GetArgVal(ctx, rep);
+	rep =
+      RedisModule_Call(ctx, "LINDEX", "ss", argv[2], index);
+    RMUTIL_ASSERT_NOERROR(rep);
+    long long element2 = GetArgVal(ctx, rep);
+	long long elementsum = element1 + element2;
+    rep =
+      RedisModule_Call(ctx, "RPUSH", "sl", argv[3], elementsum);
+    RMUTIL_ASSERT_NOERROR(rep);
+  }
+
+  RedisModule_ReplyWithLongLong(ctx, listLength1);
+  return REDISMODULE_OK;
+}
+
+// Method to insert the value of a key into a list from left using the key name.
+
+int Llkpush(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+
+ // we must have at least 4 args
+  if (argc < 3) {
+    return RedisModule_WrongArity(ctx);
+  }
+
+  // init auto memory for created strings
+  RedisModule_AutoMemory(ctx);
+  
+  for(long long i = 2;i < argc;i++) {
+  	RedisModuleCallReply *rep =
+      RedisModule_Call(ctx, "GET", "s", argv[i]);
+    RMUTIL_ASSERT_NOERROR(rep);
+    RedisModuleString *value = RedisModule_CreateStringFromCallReply(rep);
+	rep =
+      RedisModule_Call(ctx, "LPUSH", "ss", argv[1], value);
+    RMUTIL_ASSERT_NOERROR(rep);
+  }
+
+  RedisModule_ReplyWithLongLong(ctx, argc-2);
+  return REDISMODULE_OK;
+}
+
+// Method to insert the value of a key into a list from right using the key name.
+
+int Lrkpush(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+
+ // we must have at least 4 args
+  if (argc < 3) {
+    return RedisModule_WrongArity(ctx);
+  }
+
+  // init auto memory for created strings
+  RedisModule_AutoMemory(ctx);
+  
+  for(long long i = 2;i < argc;i++) {
+  	RedisModuleCallReply *rep =
+      RedisModule_Call(ctx, "GET", "s", argv[i]);
+    RMUTIL_ASSERT_NOERROR(rep);
+    RedisModuleString *value = RedisModule_CreateStringFromCallReply(rep);
+	rep =
+      RedisModule_Call(ctx, "RPUSH", "ss", argv[1], value);
+    RMUTIL_ASSERT_NOERROR(rep);
+  }
+
+  RedisModule_ReplyWithLongLong(ctx, argc-2);
+  return REDISMODULE_OK;
+}
 
 int RedisModule_OnLoad(RedisModuleCtx *ctx) {
 
@@ -318,6 +597,36 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx) {
   }
 
   if (RedisModule_CreateCommand(ctx, "arith.lincrby", LincrBy, "readonly",
+                                1, 1, 1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+
+  if (RedisModule_CreateCommand(ctx, "arith.lrsum", Lrsum, "readonly",
+                                1, 1, 1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+
+  if (RedisModule_CreateCommand(ctx, "arith.lrmax", Lrmax, "readonly",
+                                1, 1, 1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+
+  if (RedisModule_CreateCommand(ctx, "arith.lrmin", Lrmin, "readonly",
+                                1, 1, 1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+
+  if (RedisModule_CreateCommand(ctx, "arith.llsum", Llsum, "readonly",
+                                1, 1, 1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+
+  if (RedisModule_CreateCommand(ctx, "arith.llkpush", Llkpush, "readonly",
+                                1, 1, 1) == REDISMODULE_ERR) {
+    return REDISMODULE_ERR;
+  }
+
+  if (RedisModule_CreateCommand(ctx, "arith.lrkpush", Lrkpush, "readonly",
                                 1, 1, 1) == REDISMODULE_ERR) {
     return REDISMODULE_ERR;
   }
